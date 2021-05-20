5e93108ce5a5ac43ef296391315e5fac88d28a21
szymel2020 <szymon.chala@gmail.com>
2021-05-20 21:50:29 +0200

    create <p>

--- Wstęp/wstep.js
+++ Wstęp/wstep.js
@@ -6,3 +6,17 @@ function putDay() {
     var nameOfDay = prompt('Please put day, which you whish');
     day.textContent = 'Today is:' + nameOfDay;
 }
+
+// tworzenie paragrafu
+
+function stworzParagraf() {
+    var para = document.createElement('p');
+    para.textContent = 'Kliknąłeś przycisk';
+    document.body.appendChild(para);
+}
+
+var przyciski = document.querySelectorAll('button');
+
+for (var i = 0; i < przyciski.length; i++) {
+    przyciski[i].addEventListener('click', stworzParagraf);
+}
